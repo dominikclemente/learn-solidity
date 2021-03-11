72548c9231c924680b577b8ecc57248bf616a1c4
Dominik <dominik.clemente@gmail.com>
2021-03-11 20:29:58 +0100

    add(reentrancy)

---
+++ smart-contract-201-iot/security/Re-entrancy.sol
@@ -0,0 +1,26 @@
+pragma solidity 0.8.0;
+
+/* 
+    IMPORTANT DESIGN PATTERN IN SOLIDITY
+    CHECKS -> EFFECTS (MANAGING STATE) -> INTERACTION
+*/
+
+contract test {
+
+    mapping (address  => uint) balance;
+
+    function withdraw() public {
+        require(balance[msg.sender] > 0); // CHECK
+
+        uint toTransfer = balance[msg.sender];
+        address payable owner = payable(msg.sender);
+        balance[msg.sender] = 0; // EFFECT
+
+        bool success = owner.send(toTransfer);
+
+        if (!success) {
+            balance[msg.sender] = toTransfer;
+        }
+    }
+
+}
