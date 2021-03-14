c4ea303a2a7019bf6f366591f8563ba7c37a0f6f
Dominik <dominik.clemente@gmail.com>
2021-03-14 12:00:18 +0100

    extend(ERC20)

--- smart-contract-201-iot/token/contracts/Token.sol
+++ smart-contract-201-iot/token/contracts/Token.sol
@@ -1,9 +1,10 @@
 pragma solidity >=0.6.0 <0.8.0;
 
-import '../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '../node_modules/@openzeppelin/contracts/token/ERC20/ERC20Capped.sol';
+import '../node_modules/@openzeppelin/contracts/access/Ownable.sol';
 
-contract DCToken is ERC20 {
-    constructor() ERC20("DeCentral", "DC"){
+contract DCToken is ERC20Capped, Ownable {
+    constructor() ERC20("DeCentral", "DC") ERC20Capped(21000001){
         _mint(msg.sender, 1000);
     }
 }
